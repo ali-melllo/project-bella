61a0944e0050506fc1d066bec5a98dca7ec05604
ali-melllo <alimelllo32@gmail.com>
2025-07-18 16:59:11 +0330

    ui fix

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -240,7 +240,7 @@ export function LandingPage() {
 
       <section className="relative min-h-screen w-full flex z-20 items-center overflow-x-hidden md:pt-16 mb-20 px-3 md:px-20">
 
-        <div className="w-full absolute flex justify-center top-28 md:top-24 z-20">
+        <div className="w-full absolute flex justify-center top-24 z-20">
           <Badge
             variant="secondary"
             className="md:px-8 flex justify-center w-auto mx-auto items-center py-2  rounded-3xl text-xs md:text-base font-medium bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]"
