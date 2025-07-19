3aea561722a5716f7eaf0d0cbfcc1430a37bedc7
ali-melllo <alimelllo32@gmail.com>
2025-07-19 14:46:44 +0330

    assitance ui fix

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -239,7 +239,7 @@ export function LandingPage() {
 
     <>
 
-      <div className="fixed z-50 bottom-7 right-7 ">
+      <div className="fixed z-50 bottom-7 right-7 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] rounded-2xl">
         <FixedAIAssistant showBubble={false}/>
       </div>
 
